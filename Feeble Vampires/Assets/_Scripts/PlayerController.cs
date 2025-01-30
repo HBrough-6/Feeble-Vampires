0ea634870faa0c6650042fd670214d1bc02e6e95
jchagerman <116826038+jchagerman@users.noreply.github.com>
2025-01-30 08:53:17 -0800

    Movement and Confined to Board

--- Feeble Vampires/Assets/_Scripts/PlayerController.cs
+++ Feeble Vampires/Assets/_Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public bool upBlocked;
+    public bool leftBlocked;
+    public bool downBlocked;
+    public bool rightBlocked;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,53 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        blockedMovement();
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (upBlocked == false)
+                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (leftBlocked == false)
+                transform.position = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (downBlocked == false)
+                transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (rightBlocked == false)
+                transform.position = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
+        }
+    }
+
+    public void blockedMovement()
+    {
+        if (transform.position.z == 8)
+        {
+            upBlocked = true;
+        }
+        else upBlocked = false;
+
+        if (transform.position.x == -7)
+        {
+            leftBlocked = true;
+        }
+        else leftBlocked = false;
+
+        if (transform.position.z == -7)
+        {
+            downBlocked = true;
+        }
+        else downBlocked = false;
+
+        if (transform.position.x == 8)
+        {
+            rightBlocked = true;
+        }
+        else rightBlocked = false;
     }
 }
