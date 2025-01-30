45a9019b614a5a3aa6f3811c5b71af65f4f9b814
jchagerman <116826038+jchagerman@users.noreply.github.com>
2025-01-30 09:35:55 -0800

    Added End Point

---
+++ Feeble Vampires/Assets/_Scripts/MovementManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementManager : MonoBehaviour
+{
+    public GameObject player;
+    public int spaceCap;
+    int distance;
+    public GameObject endPoint;
+
+    public List<Vector2> pathPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spaceCap = 2;
+        distance = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (distance < 1) endPoint.transform.position = new Vector3(endPoint.transform.position.x, -1, endPoint.transform.position.z);
+        else endPoint.transform.position = new Vector3(endPoint.transform.position.x, 1, endPoint.transform.position.z);
+    }
+
+
+}
