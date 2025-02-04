c99d2f019c5b0ac23ac5536f2fdc61b996c59cca
jchagerman <116826038+jchagerman@users.noreply.github.com>
2025-02-04 08:25:28 -0800

    Finished Movement

--- Feeble Vampires/Assets/_Scripts/MovementManager.cs
+++ Feeble Vampires/Assets/_Scripts/MovementManager.cs
@@ -6,7 +6,7 @@ public class MovementManager : MonoBehaviour
 {
     public GameObject player;
     public int spaceCap;
-    int distance;
+    public int distance;
     public GameObject endPoint;
 
     public List<Vector2> pathPoints;
@@ -22,35 +22,130 @@ public class MovementManager : MonoBehaviour
     {
         spaceCap = 2;
         distance = 0;
+        initializeOrigin();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (distance < 1) endPoint.transform.position = new Vector3(endPoint.transform.position.x, -1, endPoint.transform.position.z);
+        if (distance < 1) endPoint.transform.position = new Vector3
+                (endPoint.transform.position.x, -1, endPoint.transform.position.z);
         else endPoint.transform.position = new Vector3(endPoint.transform.position.x, 1, endPoint.transform.position.z);
 
         movementBlocked();
 
         if (Input.GetKeyDown(KeyCode.W))
         {
+            if (distance > 0
+                && pathPoints[distance - 1] == new Vector2(endPoint.transform.position.x, endPoint.transform.position.z + 1))
+            {
+                endPoint.transform.position = new Vector3
+                    (pathPoints[distance - 1].x, endPoint.transform.position.y, pathPoints[distance - 1].y);
+                removePathPoint();
+            }
             if (!upBlocked)
             {
-
+                endPoint.transform.position = new Vector3
+                    (endPoint.transform.position.x, endPoint.transform.position.y, endPoint.transform.position.z + 1);
+                addPathPoint();
             }
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (distance > 0
+                && pathPoints[distance - 1] == new Vector2(endPoint.transform.position.x - 1, endPoint.transform.position.z))
+            {
+                endPoint.transform.position = new Vector3
+                    (pathPoints[distance - 1].x, endPoint.transform.position.y, pathPoints[distance - 1].y);
+                removePathPoint();
+            }
+            if (!leftBlocked)
+            {
+                endPoint.transform.position = new Vector3
+                    (endPoint.transform.position.x - 1, endPoint.transform.position.y, endPoint.transform.position.z);
+                addPathPoint();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (distance > 0
+                && pathPoints[distance - 1] == new Vector2(endPoint.transform.position.x, endPoint.transform.position.z - 1))
+            {
+                endPoint.transform.position = new Vector3
+                    (pathPoints[distance - 1].x, endPoint.transform.position.y, pathPoints[distance - 1].y);
+                removePathPoint();
+            }
+            if (!downBlocked)
+            {
+                endPoint.transform.position = new Vector3
+                    (endPoint.transform.position.x, endPoint.transform.position.y, endPoint.transform.position.z - 1);
+                addPathPoint();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (distance > 0
+                && pathPoints[distance - 1] == new Vector2(endPoint.transform.position.x + 1, endPoint.transform.position.z))
+            {
+                endPoint.transform.position = new Vector3
+                    (pathPoints[distance - 1].x, endPoint.transform.position.y, pathPoints[distance - 1].y);
+                removePathPoint();
+            }
+            if (!rightBlocked)
+            {
+                endPoint.transform.position = new Vector3
+                    (endPoint.transform.position.x + 1, endPoint.transform.position.y, endPoint.transform.position.z);
+                addPathPoint();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            resetMovement();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            submitMovement();
         }
     }
 
     public void movementBlocked()
     {
-        if (endPoint.transform.position.z == 8) upBlocked = true;
+        if (distance == spaceCap)
+        {
+            upBlocked = true;
+            leftBlocked = true;
+            downBlocked = true;
+            rightBlocked = true;
+        }
+        else
+        {
+            if (endPoint.transform.position.z == 8) upBlocked = true;
+            else upBlocked = false;
 
-        if (endPoint.transform.position.x == -7) leftBlocked = true;
+            if (endPoint.transform.position.x == -7) leftBlocked = true;
+            else leftBlocked = false;
 
-        if (endPoint.transform.position.z == -7) downBlocked = true;
+            if (endPoint.transform.position.z == -7) downBlocked = true;
+            else downBlocked = false;
 
-        if (endPoint.transform.position.x == 8) rightBlocked = true;
+            if (endPoint.transform.position.x == 8) rightBlocked = true;
+            else rightBlocked = false;
+        }
+    }
 
+    public void resetMovement()
+    {
+        endPoint.transform.position = new Vector3(pathPoints[0].x, endPoint.transform.position.y, pathPoints[0].y);
+        initializeOrigin();
+    }
+
+    public void submitMovement()
+    {
+        player.transform.position = new Vector3
+            (endPoint.transform.position.x, player.transform.position.y, endPoint.transform.position.z);
+        initializeOrigin();
     }
 
     public void initializeOrigin()
@@ -58,5 +153,18 @@ public class MovementManager : MonoBehaviour
         pathPoints.Clear();
         newPoint = new Vector2(endPoint.transform.position.x, endPoint.transform.position.z);
         pathPoints.Add(newPoint);
+        distance = 0;
+    }
+
+    public void addPathPoint()
+    {
+        newPoint = new Vector2(endPoint.transform.position.x, endPoint.transform.position.z);
+        pathPoints.Add(newPoint);
+        distance++;
+    }
+    public void removePathPoint()
+    {
+        pathPoints.RemoveAt(distance);
+        distance--;
     }
 }
