faf8b4d0b95e063d02c2d15550c66bcc9fc3b1d9
HBrough-6 <130100349+HBrough-6@users.noreply.github.com>
2025-04-09 23:50:28 -0700

    Item information

--- Feeble Vampires/Assets/_Scripts/Misc/SkillSO.cs
+++ Feeble Vampires/Assets/_Scripts/Misc/SkillSO.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 
-public class ItemSO : ScriptableObject
+
+[CreateAssetMenu(fileName = "SkillObject", menuName = "ScriptableObjects/SkillObject")]
+public class SkillSO : ScriptableObject
 {
     [field: SerializeField] public string DisplayName { get; private set; }
     [field: SerializeField] public Sprite Icon { get; private set; }
