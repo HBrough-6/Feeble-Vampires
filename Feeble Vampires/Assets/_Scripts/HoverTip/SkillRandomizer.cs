44a63dfe90e230c64155b23ade806d2eecc0274c
jchagerman <116826038+jchagerman@users.noreply.github.com>
2025-04-10 00:22:50 -0700

    Changes Image Randomly

--- Feeble Vampires/Assets/_Scripts/HoverTip/SkillRandomizer.cs
+++ Feeble Vampires/Assets/_Scripts/HoverTip/SkillRandomizer.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class SkillRandomizer : MonoBehaviour
 {
     //public TextMeshProUGUI skill1Text;
     //public TextMeshProUGUI skill2Text;
-    public List<string> skills;
+    public GameObject skill1Button;
+    public GameObject skill2Button;
+    //public List<string> skills;
+    public List<SkillSO> skills;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +21,12 @@ public class SkillRandomizer : MonoBehaviour
 
         //if (skill2Text == skill1Text) skill2Text.text = skills[Random.Range(0, skills.Count - 1)];
 
+        skill1Button.transform.Find("Image").gameObject.GetComponent<Image>().sprite = skills[Random.Range(0, skills.Count - 1)].Icon;
+        skill2Button.transform.Find("Image").gameObject.GetComponent<Image>().sprite = skills[Random.Range(0, skills.Count - 1)].Icon;
 
+        if (skill2Button.transform.Find("Image").gameObject.GetComponent<Image>().sprite ==
+            skill1Button.transform.Find("Image").gameObject.GetComponent<Image>().sprite)
+            skill2Button.transform.Find("Image").gameObject.GetComponent<Image>().sprite = skills[Random.Range(0, skills.Count - 1)].Icon;
     }
 
     // Update is called once per frame
