568c2cd299ce3f42771bde659f36708461ebdd2d
cbwnyx <mominepoomp@gmail.com>
2025-04-23 20:17:53 -0700

    tried fixing skill selection, not fully functioning

--- Feeble Vampires/Assets/_Scripts/UIManager.cs
+++ Feeble Vampires/Assets/_Scripts/UIManager.cs
@@ -29,7 +29,7 @@ public class UIManager : MonoBehaviour
 
     public void UpdateXP(int amount)
     {
-        xpText.text = " " + amount;
+        xpText.text = "XP: " + amount;
     }
 
     public void makeMap(bool canEcholocate)
