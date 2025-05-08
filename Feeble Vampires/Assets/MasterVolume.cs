3cadb68907f69a58b4f065b94ebc13f242afb99f
HBrough-6 <130100349+HBrough-6@users.noreply.github.com>
2025-05-07 23:03:20 -0700

    Removed Settings from Pause Menu

--- Feeble Vampires/Assets/MasterVolume.cs
+++ Feeble Vampires/Assets/MasterVolume.cs
@@ -5,19 +5,12 @@ public class MasterVolume : MonoBehaviour
 {
     [SerializeField] Slider soundSlider;
 
-    public float volume;
 
     // Start is called before the first frame update
     void Start()
     {
-        PlayerPrefs.SetFloat("SavedMasterVolume", 1);
+        PlayerPrefs.SetFloat("SavedMasterVolume", 0.5f);
         AdjustVolume(PlayerPrefs.GetFloat("SavedMasterVolume", 1));
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        volume = AudioListener.volume;
     }
 
     public void SetVolumeFromSlider()
